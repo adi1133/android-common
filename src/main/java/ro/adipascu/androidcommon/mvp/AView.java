350594e3a34a65e8f2e483dd3977a2d78667f24d
a.pascu <a.pascu@rosoftlab.net>
2015-05-22 05:47:22 +0300

    update mvp pattern and fragment lifecycle to better handle nested presenters

--- src/main/java/ro/adipascu/androidcommon/mvp/AView.java
+++ src/main/java/ro/adipascu/androidcommon/mvp/AView.java
@@ -1,9 +1,12 @@
 package ro.adipascu.androidcommon.mvp;
 
+import android.support.annotation.NonNull;
+
 /**
  * Created by Adi Pascu on 4/27/15.
  * Email mail@adipascu.ro
  */
 public interface AView<P extends APresenter> {
+    @NonNull
     P getPresenter();
 }
--- src/main/java/ro/adipascu/androidcommon/mvp/ActivityAView.java
+++ src/main/java/ro/adipascu/androidcommon/mvp/ActivityAView.java
@@ -9,21 +9,31 @@ import ro.adipascu.androidcommon.MaterialActivity;
  * Email mail@adipascu.ro
  */
 public abstract class ActivityAView<P extends APresenter> extends MaterialActivity implements AView<P> {
+    boolean called = false;
+
+    /**
+     * Call this method when the views are set up and {@link #getPresenter()} will not return null
+     */
+    protected final void viewsReady() {
+        called = true;
+        //noinspection unchecked
+        getPresenter().attach(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (called)
+            throw new UnsupportedOperationException("call viewsReady after super.onCreate");
     }
-
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        //noinspection unchecked
-        getPresenter().attach(this);
-
+    protected void onStart() {
+        super.onStart();
+        if (!called)
+            throw new UnsupportedOperationException("call viewsReady before onStart");
+        called = false;
     }
-
 
     @Override
     protected void onDestroy() {
