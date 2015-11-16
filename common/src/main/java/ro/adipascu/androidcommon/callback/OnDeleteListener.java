91b963257873c3a68edaa0c99e5980ff7fcec92b
Adi Pascu <mail@adipascu.ro>
2015-11-16 16:37:48 +0200

    Remove deprecated code and unused dependencies

--- common/src/main/java/ro/adipascu/androidcommon/callback/OnDeleteListener.java
+++
@@ -1,9 +0,0 @@
-package ro.adipascu.androidcommon.callback;
-
-/**
- * Created by Adi Pascu on 5/13/2015.
- * Email mail@adipascu.ro
- */
-public interface OnDeleteListener<T> extends OnSelectListener<T> {
-    void delete(T item);
-}
--- common/src/main/java/ro/adipascu/androidcommon/callback/OnSelectListener.java
+++
@@ -1,9 +0,0 @@
-package ro.adipascu.androidcommon.callback;
-
-/**
- * Created by Adi Pascu on 5/13/2015.
- * Email mail@adipascu.ro
- */
-public interface OnSelectListener<T> {
-    void select(T item);
-}
